7f1bbcbbe9127b2009f5859758be5fde4106553b
root <root@debian7.cloudatcost.com>
2014-01-16 15:12:24 -0500

    rubyraptor commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = '006c21772d56f1dbfb4dd41079b26c1e780bae94b58f6a36f65e5972ed17f9f47ad3846ff186e146b8b2331c19487fca622500d4f4b3054b12e6ec7fed33ef1d'
