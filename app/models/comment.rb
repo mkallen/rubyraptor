92f2c21fd86efa9021ca66dcdb42cc0a198745ee
mkallen <allenmkat@live.com>
2014-01-21 09:47:08 -0600

    rubymine-- validation updates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates_associated :post
   validates :commenter, presence:true, length: {minimum:5}
+  validates :body, presence:true, length: {minimum:15}
 
 end
