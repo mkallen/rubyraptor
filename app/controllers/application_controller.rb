0dc017702a930999c01ab347a3816c6b72ff4c16
mkallen <allenmkat@live.com>
2014-01-21 09:04:50 -0600

    rubymine--  error alert works, trying conditional html rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+
+
+class Object
+  def nil_zero?
+    self.nil? || self == 0
+  end
+end
